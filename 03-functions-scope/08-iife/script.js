143c37e02a706b6ee00b127c6c03cc4ba682c4bf
Sercan <lkse5728@gmail.com>
2023-04-14 08:17:34 +0300

    javascript-sandbox

---
+++ 03-functions-scope/08-iife/script.js
@@ -0,0 +1,10 @@
+(function () {
+  const user = 'John';
+  console.log(user);
+  const hello = () => console.log('Hello from the IIFE');
+  hello();
+})();
+
+(function (name) {
+  console.log('Hello ' + name);
+})('Shawn');
